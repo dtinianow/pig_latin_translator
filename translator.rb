fc9788773f20f3a1ba3540c4a9b3aae89ee43dcd
David Tinianow <david.tinianow@gmail.com>
2017-02-17 13:11:42 -0700

    Write more tests and psuedocode for to pig latin method

--- translator.rb
+++ translator.rb
@@ -1,4 +1,13 @@
 class Translator
+  LETTER_TYPES = {
+    a:
+  }
+
   def self.to_pig_latin(input)
+    # split string
+    # iterate over each word
+    # check if first letter is consonant or vowel
+    # apply correct transformation
+    # join array
   end
 end
--- translator_test.rb
+++ translator_test.rb
@@ -4,8 +4,25 @@ require './translator'
 
 class TranslatorTest < Minitest::Test
   def test_it_can_translate_word_starting_with_consonant_to_pig_latin
-    actual = Translator.to_pig_latin('pig')
+    input = 'pig'
+    actual = Translator.to_pig_latin(input)
     expected = 'igpay'
+
+    assert_equal actual, expected
+  end
+
+  def test_it_can_translate_word_starting_with_vowel_to_pig_latin
+    input = 'arrow'
+    actual = Translator.to_pig_latin(input)
+    expected = 'arrowway'
+
+    assert_equal actual, expected
+  end
+
+  def test_it_can_translate_multiple_words_to_pig_latin
+    input = 'pig arrow'
+    actual = Translator.to_pig_latin(input)
+    expected = 'igpay arrowway'
 
     assert_equal actual, expected
   end
